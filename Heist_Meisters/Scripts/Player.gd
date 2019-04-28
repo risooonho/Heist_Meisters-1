bd573d12d7a10f8885bbbcc555f7eb305d6b0abe
1ntrepid MAXIMUM+ <blackmathsavior@gmail.com>
2019-04-28 12:07:53 -0700

    [1-5] Light2D Nodes
    NOTE: Moved Player.tscn and Character.tscn to appropriate subfolder.

--- Heist_Meisters/Scripts/Player.gd
+++ Heist_Meisters/Scripts/Player.gd
@@ -21,3 +21,10 @@ func update_motion(delta):
 	else:
 		motion.x = lerp(motion.x, 0, FRICTION)
 		motion.y = lerp(motion.y, 0, FRICTION)
+
+func _input(event):
+	if Input.is_action_just_pressed("ui_select"):
+		toggle_torch()
+
+func toggle_torch():
+	$Torch.enabled = !$Torch.enabled #Udemy Kit's brilliant toggle
