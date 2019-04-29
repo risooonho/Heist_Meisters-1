90ffa497f74de8096bcf1b6b6cfcecb2781824d2
1ntrepid MAXIMUM+ <blackmathsavior@gmail.com>
2019-04-29 11:36:00 -0700

    [1-6] Making a Security Camera

--- Heist_Meisters/Scripts/Player.gd
+++ Heist_Meisters/Scripts/Player.gd
@@ -22,9 +22,5 @@ func update_motion(delta):
 		motion.x = lerp(motion.x, 0, FRICTION)
 		motion.y = lerp(motion.y, 0, FRICTION)
 
-func _input(event):
-	if Input.is_action_just_pressed("ui_select"):
-		toggle_torch()
-
-func toggle_torch():
-	$Torch.enabled = !$Torch.enabled #Udemy Kit's brilliant toggle
+#func toggle_torch():
+#	$Torch.enabled = !$Torch.enabled #Udemy Kit's brilliant toggle
